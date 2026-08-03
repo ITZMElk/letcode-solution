423624d6fd92473fffc312edc85805c118d161b4
RUDRAPRATAPSINGH DIXIT <rudra123456dixit@gmail.com>
2026-08-04 00:48:15 +0530

    Add solution: add-two-numbers

---
+++ LeetCode/add-two-numbers/solution.java
@@ -0,0 +1,10 @@
+// Source: https://leetcode.com/problems/add-two-numbers/
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        return dummy.next;
+    
+    }
+}
